6d2338c47542b80ce39a3c454bcf71024c24c7df
Angad Hundal <angadpalhundal@gmail.com>
2026-08-29 21:18:07 -0600

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        joined = ";".join(strs)
+        return joined
+
+    def decode(self, s: str) -> List[str]:
+        if s == "":
+            return [""]
+        elif s== None:
+            return []
+        d_list = s.split(";")
+        return d_list 
