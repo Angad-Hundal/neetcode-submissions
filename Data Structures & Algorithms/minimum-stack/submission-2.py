1acccb123d0bff782990bab8dc27741107cf5568
Angad Hundal <angadpalhundal@gmail.com>
2026-08-30 14:23:23 -0600

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,28 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+        if self.min_stack and val < self.min_stack[-1]:
+            self.min_stack.append(val)
+        
+
+    def pop(self) -> None:
+        if self.stack:
+            num = self.stack[-1]
+            if self.min_stack and self.min_stack[-1] == num:
+                self.min_stack.pop(-1)
+        
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+        
