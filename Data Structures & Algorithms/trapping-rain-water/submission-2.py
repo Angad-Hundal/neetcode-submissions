a082d398eba38daf7a5e62c79a310354af24a59d
Angad Hundal <angadpalhundal@gmail.com>
2026-09-11 20:22:55 -0600

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    # make an array max_left
+    # make an array max_right
+    # iterate through the heights
+    # and keep on storing max in the arrays 
+    # two for loops
+
+    # then iterate through each index
+    # find the area by
+    # min(left, right) - height
+    def trap(self, height: List[int]) -> int:
+        
+        max_left = []
+        max_right = []
+
+        max_num = 0
+        for idx, num in enumerate(height):
+            max_left.append(max_num)
+
+            if num > max_num:
+                max_num = num
+        
+        max_num = 0
+        for idx, num in enumerate(height[::-1]):
+            max_right.append(max_num)
+
+            if num> max_num:
+                max_num = num
+        max_right = max_right[::-1]
+        
+        area = 0
+        for idx, h in enumerate(height):
+            area += min(max_left[idx], max_right[idx]) - h
+        
+        return area
+
+        
+
