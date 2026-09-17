3bdc30f533f3dc7b7bdea8f6ba7362a3cdc70629
Angad Hundal <angadpalhundal@gmail.com>
2026-09-16 21:46:10 -0600

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        count1 = {}
+        for c in s1:
+            count1[c] = 1 + count1.get(c, 0)
+        
+        # num of unique chars
+        need = len(count1)
+
+        for left in range(len(s2)):
+            count2 = {}
+            cur = 0
+
+            for right in range(left, len(s2)):
+                count2[s2[right]] = 1 + count2.get(s2[right], 0)
+                
+                # is there in count2 but not in count 1
+                # extra character
+                if count1.get(s2[right], 0) < count2[s2[right]]:
+                    break
+
+                # how many unqiue char have 
+                # reached their required freq
+                elif count1.get(s2[right], 0) == count2[s2[right]]:
+                    cur += 1
+                
+                if cur == need:
+                    return True
+        
+        return False
